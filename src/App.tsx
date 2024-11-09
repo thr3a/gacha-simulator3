aaa2148620b3e3a6dccdef143c40e28d61a068b8
thr3a <thr3a@example.com>
2024-11-09 10:04:18 +0900

    add:

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
-import { theme } from "./theme";
-import { Welcome } from "./Welcome/Welcome";
-import { ColorSchemeToggle } from "./ColorSchemeToggle/ColorSchemeToggle";
-
+import '@mantine/core/styles.css';
+import { Anchor, Container, MantineProvider, Title } from '@mantine/core';
+import { GachaForm } from './main/Form';
+import { theme } from './theme';
 export default function App() {
   return (
     <MantineProvider theme={theme}>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Container>
+        <Anchor href='/'>
+          <Title mt={'md'} order={2}>
+            ガチャシュミレーター
+          </Title>
+        </Anchor>
+
+        <Title order={6} mb={'md'} c={'dimmed'}>
+          スマホゲームなどのガチャ確率計算ツール
+        </Title>
+
+        <GachaForm />
+      </Container>
     </MantineProvider>
   );
 }
